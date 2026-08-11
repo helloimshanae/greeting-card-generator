b437793968b4edae2e092bd3678dc16027bf4178
global <sahadeo.shanae@gmail.com>
2026-08-10 21:40:23 -0400

    name the card block

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -2,6 +2,10 @@ let name = prompt("Your Name:");
 let adjective = prompt("Adjective:");
 let noun = prompt("Noun:");
 
-console.log("Meow " + name + ",");
-console.log("Wubzy has officially declared you " + adjective + " enough to be responsible for the " + noun + "!");
-console.log("DO NOT disappoint the cat.");
+function makeCard() {
+  console.log("Meow " + name + ",");
+  console.log("Wubzy has officially declared you " + adjective + " enough to be responsible for the " + noun + "!");
+  console.log("DO NOT disappoint the cat.");
+}
+
+makeCard();
