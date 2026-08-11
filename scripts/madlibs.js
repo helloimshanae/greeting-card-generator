fefe62f191a75b44e68a6c0c5cd47a36d0141f95
global <sahadeo.shanae@gmail.com>
2026-08-10 21:29:46 -0400

    mad libs v0 asks and answers

---
+++ scripts/madlibs.js
@@ -0,0 +1,7 @@
+let name = prompt("Your Name:");
+let adjective = prompt("Adjective:");
+let noun = prompt("Noun:");
+
+console.log("Meow " + name + ",");
+console.log("Wubzy has officially declared you " + adjective + " enough to be responsible for the " + noun + "!");
+console.log("DO NOT disappoint the cat.");
