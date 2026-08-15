5afadcd5eadf44b5856f3b7a09d3e987b71b4263
global <sahadeo.shanae@gmail.com>
2026-08-15 00:12:55 -0400

    speed and stop experiments

--- scripts/tick.js
+++ scripts/tick.js
@@ -1,8 +1,15 @@
 let count = 0;
+let speed = 2000;
+let timer = setInterval(tick, speed);
+
 
 function tick() {
 	count = count + 1;
 	console.log("Tick " + count);
+	
+	if (count === 10) {
+		clearInterval(timer);
+		console.log("Timer Stops");
+	}
 }
 
-setInterval(tick, 1000);
