f3d4ca6673800080a21d35810561e7903d0227e9
global <sahadeo.shanae@gmail.com>
2026-08-15 10:26:17 -0400

    random closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,22 +1,36 @@
 // ask & store the following information
-let name = prompt("Your Name:");
+let name = prompt("\nYour Name:");
 let adjective = prompt("Adjective:");
 let noun = prompt("Noun:");
 let place = prompt("Favorite Place:");
 let food = prompt("Favorite Food:");
 
+// define closing messages
+let closings = [
+	"Meow for now.",
+	"Sincerely, the cat",
+	"Love, Wubzy",
+];
+
+// define random closing
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
 // define first card
 function makeCard() {
   console.log("\nMeow " + name + ",");
   console.log("Wubzy has officially declared you " + adjective + " enough to be responsible for the " + noun + "!");
-  console.log("You must report to " + place + " for your first day. Travel arrangements will be emailed shortly.");
+  console.log("You must report to the " + place + " for your first day. Travel arrangements will be emailed shortly.");
   console.log("Wubzy has also approved your emergency supply of " + food + " to celebrate!");
   console.log("DO NOT disappoint the cat.\n");
+  let index = randomNumber(0, closings.length - 1);
+  console.log(closings[index]);
 }
 
 // define second card
 function makeThankYouCard() {
-	console.log("Thank you " + name + "!");
+	console.log("\nThank you " + name + "!");
 	console.log("Wubzy cant wait to eat " + food + " with you.")
 }
 
